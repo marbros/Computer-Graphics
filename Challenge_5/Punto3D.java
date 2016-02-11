70a822f98ec1c3993955c5debb95ffcbed65e2c9
MarioG <enigsea@gmail.com>
2016-02-10 23:59:52 -0500

    the Punto3d class is added to challenge 5

---
+++ Challenge_5/Punto3D.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package housepaint;
+
+/**
+ *
+ * @author Anderson,Pablo,Mario
+ */
+public class Punto3D {
+    
+    private double point[];
+//    double x;
+//    double y;
+//    double z;
+    
+    public Punto3D(double x, double y,double z, double w){
+        point = new double[4];
+        point[0] = x;
+        point[1] = y;
+        point[2] = z;
+        point[3] = w;
+        
+//        this.x = x;
+//        this.y = y;
+//        this.z = z;
+    }
+
+    public double getX() {
+        return  point[0];
+    }
+
+    public double getY() {
+        return  point[1];
+    }
+
+    public double getZ() {
+        return  point[2];
+    }
+    
+    public double[] getPoint() {
+        return point;
+    }
+       
+    public void setX(double x) {
+        point[0] = x;
+    }
+    
+    public void setY(double y) {
+       point[1] = y;
+    }    
+    
+     public void setZ(double z) {
+        point[2] = z;
+    }
+     
+    public void multiMatrizPunto(Matriz3D transformacion){
+        double[] aux = new double[4];
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                 aux[i] += transformacion.getMatriz3D()[i][j]*point[j];
+            }
+        }
+        point = aux;
+    }
+}
