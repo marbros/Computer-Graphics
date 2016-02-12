54c7491e1248943a61073b08a3a9359d4929d69b
MarioG <enigsea@gmail.com>
2016-02-11 23:36:24 -0500

    the files 'Matriz3d', 'Operations' are added to challenge 5

---
+++ Challenge_5/src/Matriz3D.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package housepaint;
+
+/**
+ *
+ * @author Anderson,Pablo,Mario
+ */
+public class Matriz3D {    
+    
+    private double matriz3D[][];   
+    private double identidad[][]={{1,0,0,0},
+                                  {0,1,0,0},
+                                  {0,0,1,0},
+                                  {0,0,0,1}};
+    public Matriz3D() {
+        this.matriz3D = this.identidad;
+        
+    }
+
+    public double[][] getMatriz3D() {
+        return matriz3D;
+    }
+
+    public void setMatriz3D(double[][] matriz) {
+        this.matriz3D = matriz;
+    }
+    
+    public void multiplicarConstante(int c){
+        for (int i = 0; i < matriz3D.length; i++) {
+            for (int j = 0; j < matriz3D[i].length; j++) {
+                matriz3D[i][j] *= c;
+            }
+        }
+    }
+    
+    public void scaling3D (double x, double y,double z){        
+        this.matriz3D = this.identidad;
+        matriz3D[0][0] = x;
+        matriz3D[1][1] = y;
+        matriz3D[2][2] = z;
+    }
+    
+    public void translation3D (double x, double y,double z){        
+        this.matriz3D = this.identidad;
+        matriz3D[0][3] = x;
+        matriz3D[1][3] = y;
+        matriz3D[2][3] = z;
+    }
+    
+    public void rotation3D(int eje ,double grados){        
+        this.matriz3D = this.identidad;
+        if(eje == 1){ //x
+            this.matriz3D[1][1] = Math.cos(grados);
+            this.matriz3D[1][2] = -1 * Math.sin(grados);
+            this.matriz3D[2][1] = Math.sin(grados);
+            this.matriz3D[2][2] = Math.cos(grados);
+        }else if(eje == 2){ //y           
+            this.matriz3D[0][0] = Math.cos(grados);
+            this.matriz3D[0][2] = -1 * Math.sin(grados);
+            this.matriz3D[2][0] = Math.sin(grados);
+            this.matriz3D[2][2] = Math.cos(grados);
+        }else if(eje == 3){ //z
+            this.matriz3D[0][0] = Math.cos(grados);
+            this.matriz3D[0][1] = -1 * Math.sin(grados);
+            this.matriz3D[1][0] = Math.sin(grados);
+            this.matriz3D[1][1] = Math.cos(grados);
+        }
+    }
+}
---
+++ Challenge_5/src/Operations.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package housepaint;
+
+/**
+ *
+ * @author Anderson,Pablo,Mario
+ */
+public class Operations {
+    
+    //Multiplica 1 matriz 2D por un vector
+    public static Punto2D multiplicar(Matriz2D matriz2D, Punto2D punto2D){
+        double acum = 0;
+        double[] array = new double[3];
+        for (int i = 0; i < 3; i++) {
+            for( int j = 0; j< punto2D.getPoint().length; j++){
+                acum += matriz2D.getMatriz2D()[i][j] * punto2D.getPoint()[j];
+            }
+            array[i] = acum;
+            acum = 0;
+        }       
+        Punto2D res = new Punto2D(array[0], array[1], array[2]);
+        
+        return res;
+    }
+    
+    //Multiplica 1 matriz 3D por un vector
+    public static Punto3D multiplicar(Matriz3D matriz3D, Punto3D punto3D){
+        double acum = 0;
+        double[] array = new double[4];
+        for(int i = 0; i < 4; i++){
+            for (int j = 0; j < punto3D.getPoint().length; j++) {
+                acum += matriz3D.getMatriz3D()[i][j] * punto3D.getPoint()[j];
+            }
+            array[i] = acum;
+            acum = 0;
+        }
+        
+        Punto3D res = new Punto3D(array[0], array[1], array[2], array[3]);
+        return res;
+    }
+    
+    //Multiplica 2 matrices 2D
+    public double[][] multi_Matriz2D(Matriz2D matriz1, Matriz2D matriz2){
+        double[][] res = new double[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                res[i][j] = 0;
+                for (int k = 0; k < 10; k++) {
+                    res[i][j] += matriz1.getMatriz2D()[i][k] * matriz2.getMatriz2D()[k][j];
+                }
+            }
+        }
+        return res;
+    }
+    
+    //Multiplica 2 matrices 3D
+    public double[][] multi_Matriz3D(Matriz3D matriz1, Matriz3D matriz2){       
+     
+        double[][] res = new double[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                res[i][j] = 0;
+                for (int k = 0; k < 10; k++) {
+                    res[i][j] += matriz1.getMatriz3D()[i][k] * matriz2.getMatriz3D()[k][j];
+                }
+            }
+        }
+        return res;
+    }
+    
+    //Suma 2 matrices 2D
+    public double[][] sumaMatriz2D(Matriz2D matriz1, Matriz2D matriz2){     
+        double[][] res = new double[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                res[i][j] = matriz1.getMatriz2D()[i][j] + matriz2.getMatriz2D()[i][j];
+            }
+        }        
+        return res;
+    }
+    
+    //Suma 2 matrices 3D
+    public double[][] sumaMatrix3D(Matriz3D matriz1, Matriz3D matriz2){     
+        double[][] res = new double[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                res[i][j] = matriz1.getMatriz3D()[i][j] + matriz2.getMatriz3D()[i][j];
+            }
+        }        
+        return res;
+    }
+}
