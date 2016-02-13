ece0f3985067030cc5ccc1fd448b570ceee0c10c
MarioG <enigsea@gmail.com>
2016-02-13 14:37:28 -0500

    the class 'punto2d' is added

---
+++ Challenge_2/clipping/Sutherland/punto2D.java
@@ -0,0 +1,20 @@
+package clipping.sutherland;
+
+public class punto2D {
+   private int x;
+   private int y;
+
+    public punto2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+   
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
