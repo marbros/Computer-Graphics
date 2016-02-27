b991da4bad484db35d7fa5377ad4c812e87ed8db
MarioG <enigsea@gmail.com>
2016-02-26 23:36:17 -0500

    the class 'Punto' is added

---
+++ Challenge_6/Punto.java
@@ -0,0 +1,43 @@
+package escena;
+
+public class Punto {
+    double x;
+    double y;
+    double z;
+
+    public Punto(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Punto{" + "x=" + x + "y=" + y + "z=" + z + '}';
+    }
+
+}
