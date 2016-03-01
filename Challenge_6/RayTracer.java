26bcd266321ae086dbc6d8274abc6fd89f9194b6
MarioG <enigsea@gmail.com>
2016-02-29 22:15:07 -0500

    The class 'RayTracer' is added

---
+++ Challenge_6/RayTracer.java
@@ -0,0 +1,30 @@
+package escena;
+
+public class RayTracer {
+
+    Escena escena;
+    LectorEscena lectorEscena;
+    int ancho, alto;
+    boolean DEBUG = true;
+
+    public RayTracer(int ancho, int alto) {
+        this.ancho = ancho;
+        this.alto = alto;
+        escena = new Escena();
+        lectorEscena = new LectorEscena(this, "escena.txt");
+
+    }
+
+    public void GenerarImagen(){
+        Rayo rayo;
+        double deltax = 2d / ancho;
+        double deltay = 2d / alto;
+        Imagen imagen = new Imagen(ancho, alto);
+    }
+
+    public static void main(String [] args) {
+        RayTracer rayTracer = new RayTracer(400, 400);
+        rayTracer.GenerarImagen();
+    }
+
+}
