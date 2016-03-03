6cc49f058363a2971a50a43f57069fd4562879aa
MarioG <enigsea@gmail.com>
2016-03-02 22:42:02 -0500

    the class 'Pixel' is added

---
+++ Challenge_6/Pixel.java
@@ -0,0 +1,27 @@
+package imagen;
+import escena.Color;
+
+public class Pixel {
+    // Valores entre 0 y 255
+    int r;
+    int g;
+    int b;
+
+    public Pixel(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public Pixel(Color color) {
+        this.r = (int)(color.getR() * 255d);
+        this.g = (int)(color.getG() * 255d);
+        this.b = (int)(color.getB() * 255d);
+    }
+
+    public Pixel() {
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
+    }
+}
