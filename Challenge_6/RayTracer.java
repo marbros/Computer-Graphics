c37b2606c9e7f0d76d8eb00d271567556c82eff4
MarioG <enigsea@gmail.com>
2016-03-02 22:40:38 -0500

    new changes in the class Raytracer are added

--- Challenge_6/RayTracer.java
+++ Challenge_6/RayTracer.java
@@ -22,6 +22,19 @@ public class RayTracer {
         double deltax = 2d / ancho;
         double deltay = 2d / alto;
         Imagen imagen = new Imagen(ancho, alto);
+        for (int i = 0; i<alto; i++){
+            for (int j=0; j<ancho; j++){
+                Punto punto2 = new Punto(-1 + i * deltax, 1 - j * deltay, -2d);
+                Punto punto1 = new Punto(0d, 0d, 0d);
+                MiVector vector = new MiVector(punto1, punto2);
+                rayo = new Rayo(punto1, vector);
+                //if(DEBUG) System.out.println(rayo);
+                Color color = escena.intersectarRayo(rayo);
+                Pixel pixel = new Pixel(color);
+                imagen.setPixel(i, j, pixel);
+            }
+        }
+        imagen.escribirImagen("salida.ppm");
     }
 
     public static void main(String [] args) {
