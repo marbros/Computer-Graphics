a6ad39b44c09b35a6a2f595283a9d4b4736c90b2
MarioG <enigsea@gmail.com>
2016-03-06 22:55:23 -0500

    small changes in the class 'Esfera' are added

--- Challenge_6/Math/Esfera.java
+++ Challenge_6/Math/Esfera.java
@@ -1,9 +1,6 @@
 package escena;
 
-/**
- *
- * @author LABREDES
- */
+
 public class Esfera extends Entidad{
     Punto centro;
     double radio;
@@ -11,6 +8,23 @@ public class Esfera extends Entidad{
     public Esfera(Punto centro, double radio) {
         this.centro = centro;
         this.radio = radio;
+    }
+
+    /**
+     * Finds the intersection(s) of this sphere with the ray provided as
+     * parameter
+     * @param ray Ray to check intersection with this sphere.
+     * @return An Array with the value(s) of the parameter in the ray that
+     *   determines the intersection(s) of the ray and the sphere.
+     *   If there are no intersections, array ret has 0 positions.
+     *   If there is only one intersection, array ret has 1 position.
+     *   If there are two intersections, array ret has 2 positions.
+     */
+    @Override
+    public double[] findIntersections(Ray ray) {
+        double [] ret = new double[2];
+
+        return ret;
     }
 
     @Override
