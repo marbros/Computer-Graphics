ed4ba4acc30996684d89ca0a5c1ca59e7c7498fd
MarioG <enigsea@gmail.com>
2016-03-12 23:59:02 -0500

    new changes in the class Sphere are added

--- Challenge_6/Math/Sphere.java
+++ Challenge_6/Math/Sphere.java
@@ -1,15 +1,36 @@
-package escena;
+package Math;
 
 
-public class Esfera extends Entidad{
-    Punto centro;
-    double radio;
+public class Sphere implements Intersectable {
+    /** Center point of the sphere */
+    protected Point3 center;
+    /** Radius of the sphere */
+    protected double radius;
+    
+    public static final boolean DEBUG = false;
 
-    public Esfera(Punto centro, double radio) {
-        this.centro = centro;
-        this.radio = radio;
+    /**
+     * Constructor
+     * @param center Center point of the sphere
+     * @param radius Radius of the sphere
+     */
+    public Sphere(Point3 center, double radius) {
+        this.center = center;
+        this.radius = radius;
     }
 
+    /**
+     * Computes the normal at a point on the surface of the sphere
+     * @param point point to compute the normal at
+     * @return a normalized vector from the center of the sphere to the point
+     * on the surface
+     */
+    public Vector3 computeNormal(Point3 point) {
+        Vector3 normal = new Vector3(center, point);
+        normal.normalize();
+        return normal;
+    }
+    
     /**
      * Finds the intersection(s) of this sphere with the ray provided as
      * parameter
@@ -23,15 +44,27 @@ public class Esfera extends Entidad{
     @Override
     public double[] findIntersections(Ray ray) {
         double [] ret = new double[2];
-
+        /*
+        Escribir aquí el código para hallar la intersección entre el rayo
+        y el triángulo
+        Leer el comentario en el encabezado del método para ver cómo se
+        retornan las posibles soluciones.
+        */
         return ret;
     }
+    
+    public static void main(String [] args) {
+        Sphere sphere = new Sphere(new Point3(1d, 0d, 0d), 1d);
+        Ray ray1 = new Ray(new Point3(1d, 0d, 10d), new Point3(1d, 0d, 9d));
+        double [] solutions = sphere.findIntersections(ray1);
+        
+        Ray ray2 = new Ray(new Point3(2d, 0d, 10d), new Point3(2d, 0d, 9d));
+        solutions = sphere.findIntersections(ray2);
 
-    @Override
-    public String toString() {
-        return "Esfera{" + "centro=" + centro + "radio=" + radio + '}';
+        Ray ray3 = new Ray(new Point3(3d, 0d, 10d), new Point3(3d, 0d, 9d));
+        solutions = sphere.findIntersections(ray3);
+
     }
-
-
 }
+
 
