aaa9d4336323ae27b2eecb75739a510453f6ead6
MarioG <enigsea@gmail.com>
2016-03-15 23:34:05 -0500

    new changes in the model are added

---
+++ Challenge_6/Model/AmbientLight.java
@@ -0,0 +1,14 @@
+package Model;
+
+/**
+ * Store the information for the Ambient Light
+ */
+public class AmbientLight {
+    /** MyColor of the ambient light */
+    protected MyColor color;
+
+    public AmbientLight(MyColor color) {
+        this.color = color;
+    }
+    
+}
