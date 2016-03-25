6dfc72b76aa38d24dd3c4ad7ba526464a386cd09
MarioG <enigsea@gmail.com>
2016-03-24 23:03:28 -0500

    The class segment is added

---
+++ Challenge_5/Segment.java
@@ -0,0 +1,36 @@
+package Objects;
+
+import Mathematics.Vector3D;
+import java.awt.Color;
+
+public class Segment {
+    
+    public Vector3D vector1;
+    public Vector3D vector2;
+    public Vector3D vector3;
+    public float p;
+    public Color color;
+
+    public Segment(Vector3D vector1, Vector3D vector2, Vector3D vector3, float p, Color color) {
+        this.vector1 = vector1;
+        this.vector2 = vector2;
+        this.vector3 = vector3;
+        this.color = color;
+        this.p = p;       
+    }
+    
+    public Segment(Vector3D vector1, Vector3D vector2, Vector3D vector3, float p) {
+        this.vector1 = vector1;
+        this.vector2 = vector2;
+        this.vector3 = vector3;
+        this.p = p;       
+    }
+    
+    public void hallarP(){
+        //this.p=(vector1.getZ()+vector2.getZ()+vector3.getZ())/3; 
+     
+        this.p = (float)Math.min(vector1.getZ(),vector2.getZ());//(vector1.getZ()+vector2.getZ()+vector3.getZ())/3; 
+        this.p=(float)Math.min(this.p,vector3.getZ());
+    }
+    
+}
