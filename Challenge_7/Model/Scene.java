ce58b0d44dbd0fd1c8d994d77ccddfadaf2df415
MarioG <enigsea@gmail.com>
2016-04-10 23:21:13 -0500

    new changes in the class 'Scene' are added

--- Challenge_7/Model/Scene.java
+++ Challenge_7/Model/Scene.java
@@ -3,141 +3,107 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package Scene;
 
-import Math.Intersectable;
 import java.util.ArrayList;
-import Math.Point3;
-import Math.Triangle;
 import Math.Sphere;
 import Math.Ray;
-import View.Image;
+import Math.Solutions;
+import Math.Vector4;
+import Math.Point;
 
 /**
- * Scene describing:
- * 1. The Triangle(s) to be drawn, 
- * 2. The camera (assumed to be at (0, 0, 0)
- *    Looking at (0, 0, -1)
- *    Up-vector is (0, 1, 0)
- * 3. The projection plane
- * 4. Light(s)
- * 5. Ambient Light
- * 6. Camera position
- * 
+ *
  * @author htrefftz
  */
 public class Scene {
+    // Ambient light, comes from "everywhere"
+    static AmbientLight ambientLight;
+    // Several Point Lights
+    static ArrayList<PointLight> pointLights = new ArrayList<>();
+    // Several Spheres
+    static ArrayList<Sphere> spheres = new ArrayList<>();
+    // Bacground color
+    //static final Colour BACKGROUNDCOLOR = new Colour(0.7, 0.7, 0.9);
+    static final Colour BACKGROUNDCOLOR = new Colour(0.2, 0.2, 0.2);
+    //static final Colour BACKGROUNDCOLOR = new Colour(.1, 0.74 , 1);
     
-    private static final Scene scene = new Scene();
-    
-    public static final int WIDTH_IN_PIXELS = 512;
-    public static final int HEIGHT_IN_PIXELS = 512;
-    
-    /** Objects in the scene */
-    protected ArrayList<SolidSurface> solidSurfaces;
-    /** Point lights in the scene */
-    protected ArrayList<Light> lights;
-    /** Projection plane */
-    protected ProjectionPlane projectionPlane;
-    /** Ambient light */
-    protected AmbientLight ambientLight;
-    /** Camera position */
-    protected Point3 cameraPosition;
-    /** Image created */
-    protected MyColor [][] imageToDraw;
-    
-    private final boolean DEBUG = false;
-    
-    private Scene() {  }
     /**
-     * Return the single instance of this Scene
-     * @return the singleton instance
+     * Set the ambient light in the scene
+     * @param myAmbientLight Ambient light to set
      */
-    public static Scene getInstance() {
-        return scene;
+    public static void setAmbientLight(AmbientLight myAmbientLight) {
+        ambientLight = myAmbientLight;
     }
     
     /**
-     * Constructor
-     * Allocate memory for the ArrayLists
+     * Add a point light
+     * @param myPointLight Point Light to be added 
      */
-    public void init() {
-        solidSurfaces = new ArrayList<>();
-        lights = new ArrayList<>();
-        projectionPlane = ProjectionPlane.getInstance();
-        imageToDraw = new MyColor[Scene.HEIGHT_IN_PIXELS+1][Scene.WIDTH_IN_PIXELS+1];
+    public static void addPointLight(PointLight myPointLight) {
+        pointLights.add(myPointLight);
+    }
+    
+    /**
+     * Add a Sphere
+     * @param mySphere Sphere to be added
+     */
+    public static void addSphere(Sphere mySphere) {
+        spheres.add(mySphere);
     }
 
-    public void addSolidSurface(SolidSurface solidSurface) {
-        solidSurfaces.add(solidSurface);
-        if(DEBUG) System.out.println("Added one surface");
-    }
-    
-    public void addLight(Light light) {
-        lights.add(light);
-        if(DEBUG) System.out.println("Added one light");
-    }
-    
-    public void setAmbientLight(AmbientLight ambientLight) {
-        this.ambientLight = ambientLight;
-    }
-    
-    public void setProjectionPlane(ProjectionPlane pp) {
-        projectionPlane = pp;
-    }
-    
-    public void setCameraPosition(Point3 cp) {
-        this.cameraPosition = cp;
-    }
-
-    public MyColor[][] getImageToDraw() {
-        return imageToDraw;
-    }
-    
-    public void throwRays() {
-        ProjectionPlane pp = ProjectionPlane.getInstance();
-        Point3 p0 = new Point3(0, 0, 0);
-
-        // From bottom to top
-        for(int yIndex = 0; yIndex <= Scene.HEIGHT_IN_PIXELS; yIndex++) {
-            // From left to right
-            for(int xIndex = 0; xIndex <= Scene.WIDTH_IN_PIXELS; xIndex++) {
-                Point3 p1 = pp.computePointOnPlane(xIndex, yIndex);
-                Ray ray = new Ray(p0, p1);
-                imageToDraw[yIndex][xIndex] = new MyColor(0, 0, 0);
-                
-                double depth = Double.MAX_VALUE;
-                for(SolidSurface ss: solidSurfaces) {
-                    Intersectable intersectable = ss.getIntersectable();
-                    double [] solutions = new double[0];
-                    // The object is a triangle
-                    if(intersectable instanceof Triangle) {
-                        Triangle triangle = (Triangle)(intersectable);
-                        solutions = triangle.findIntersections(ray);
-                    // the object is a sphere
-                    } else if (intersectable instanceof Sphere) {
-                        /*
-                        Escribir aquí el código para llamar el código
-                        findIntersections(ray) de la clase Sphere
-                        IMPORTANTE: si hay dos soluciones, se debe asegurar
-                        que la solución más cercana a la cámara quede
-                        en la posición 0
-                        */
-                        Sphere sphere = (Sphere)intersectable;
+    /**
+     * Compute the color of the closest object in the Scene this ray 
+     * intersects with
+     * @param ray Ray to intersect with the Scene
+     * @return 
+     */
+    public static Colour intersectRay(Ray ray) {
+        double minT = Double.MAX_VALUE;
+        Sphere closest = null;
+        // Chose the closest surface
+        for(Sphere sphere: spheres) {
+            Solutions s = Sphere.intersect(sphere, ray);
+            if(s.getNumSolutions() > 0) {
+                // ignore solutins with a t value smaller than 0,
+                // because they are behind the origin of the ray
+                if(s.getT1() > 0.01) {
+                    // Is this surface closer to the origin of the ray?
+                    if(s.getT1() < minT) {
+                        minT = s.getT1();
+                        closest = sphere;
                     }
-                    if(solutions.length > 0) {
-                        Point3 intersectionPoint = ray.evaluate(solutions[0]);
-                        if(DEBUG)
-                            System.out.println("intersectionPoint: " + intersectionPoint);
-                        if(solutions[0] < depth) {
-                            imageToDraw[yIndex][xIndex] =
-                                Illuminator.getInstance().computeColor(intersectionPoint, ss);
-                            depth = solutions[0];
-                        } 
-                    } 
                 }
             }
         }
+        if(closest != null) {
+            Colour acum  = new Colour(0, 0, 0);
+            double Ko = closest.getMaterial().Ko;
+            double Kr = closest.getMaterial().Kr;
+            double Kt = closest.getMaterial().Kt;
+            // Compute this oject's color
+            if(Ko != 0) {
+                Colour thisColor = Colour.multiply(closest.callShader(ray, minT),Ko);
+                acum = Colour.add(acum, thisColor);
+            }
+            // Compute the reflection
+            if(Kr != 0) {
+                // Find the normal at the solution
+                Point p = ray.evaluate(minT);
+                Vector4 normal = closest.computeNormal(p);
+                // create the reflection ray (reflectedRay)
+                Vector4 direction = Vector4.reflection(ray.getU(), normal);
+                Ray reflectedRay = new Ray(p, direction);
+                // send the ray to intersect with objects in the scene (Scene.intersectRay(reflectedRay))
+                // (this is where recursion takes place)
+                Colour reflectedColor = Scene.intersectRay(reflectedRay);
+                // multiply the color by the corresponding Weight (Kr) 
+                reflectedColor = Colour.multiply(reflectedColor, Kr);
+                // and add the color to acum
+                acum = Colour.add(acum, reflectedColor);
+            }
+            return acum;
+        }
+        return BACKGROUNDCOLOR;
     }
-    
 }
