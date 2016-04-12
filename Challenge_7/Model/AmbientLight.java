431c5f8f9f2fc19ad56c25d102d598a85565d9fc
MarioG <enigsea@gmail.com>
2016-04-11 21:48:57 -0500

    small changes are added to project

--- Challenge_7/Model/AmbientLight.java
+++ Challenge_7/Model/AmbientLight.java
@@ -7,6 +7,10 @@ public class AmbientLight {
     /** MyColor of the ambient light */
     protected MyColor color;
 
+    /**
+     * Constructor
+     * @param color Colour of the ambient light 
+     */
     public AmbientLight(MyColor color) {
         this.color = color;
     }
